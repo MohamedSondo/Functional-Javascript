6342669f1a9b6f857fe97d3cd9d6af064e900fd6
Monaf Corporation <epsilon@Monafs-MacBook-Pro.local>
2017-12-24 10:42:18 -0500

    first commit

---
+++ Exercise_Files/02_03/Begin/01_doif.js
@@ -0,0 +1,25 @@
+// Learning Functional Programming with Javascript
+// Chapter 02, Video 03, Exercise 01
+
+function doIf(condition, funct){
+    if(condtion){
+        funct()
+    }
+}
+const x = 1
+
+doIf (x === 1,sayXis1)
+
+
+doIf(x === "Bananas",sayXisBananas)
+
+
+doIf (x < 10 && x > 0, ayXisBetween0And10)
+
+
+function sayXis1() { console.log("x is equal to 1") }
+
+function sayXisBananas() { console.log("x is equal to 'Bananas'") }
+
+function sayXisBetween0And10() { console.log("x is between 0 and 10") }
+
